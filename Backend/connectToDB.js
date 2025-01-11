f35419f4f24ecffef66794689c1d6c8fbc594097
Prashidha Poudel <prashidha45@gmail.com>
2025-01-11 13:37:51 -0600

    Fifth Commit

--- Backend/connectToDB.js
+++ Backend/connectToDB.js
@@ -1,6 +1,7 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 export  const ConnectToDB = () =>{
+    console.log(process.env.MONGO_URI)
     mongoose.connect(process.env.MONGO_URI)
     .then(()=>console.log("Successful Connection to the database"))
     .catch((error)=>console.log("Error connecting to the database", error))
